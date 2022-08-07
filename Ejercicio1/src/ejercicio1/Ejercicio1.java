742ddef63a47560ea7870a245392638046287bec
José Guerrero <jg2112n@gmail.com>
2022-08-07 16:04:50 -0300

    ejercicio1 listo

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio1;
+import java.util.Scanner;
 
-/**
- *
- * @author jg211
- */
+
+
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+                
+        int num1, num2, num3;
+        
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        
+        System.out.println(num1 + num2);
     }
     
 }
